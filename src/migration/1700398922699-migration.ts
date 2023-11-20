5f390d45f3decbba212546895869b9dd38588db8
nem <canhnamtran216@gmail.com>
2023-11-20 11:43:31 +0700

    feat: user profile & stock query

--- src/migration/1700398922699-migration.ts
+++ src/migration/1700398922699-migration.ts
@@ -1,14 +1,14 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Migration1698686257602 implements MigrationInterface {
-  name = 'Migration1698686257602';
+export class Migration1700398922699 implements MigrationInterface {
+  name = 'Migration1700398922699';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "user"."historical_data" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "Date" TIMESTAMP WITH TIME ZONE NOT NULL, "OpenPrice" bigint NOT NULL, "HighPrice" bigint NOT NULL, "LowPrice" bigint NOT NULL, "ClosePrice" bigint NOT NULL, "AdjClosePrice" bigint NOT NULL, "Volume" bigint NOT NULL, "Symbol" text, "Change" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d6303a200d1d9dc8f3bcb857331" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "user"."historical_data" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "date" TIMESTAMP WITH TIME ZONE NOT NULL, "open_price" bigint NOT NULL, "high_price" bigint NOT NULL, "low_price" bigint NOT NULL, "close_price" bigint NOT NULL, "adj_close_price" bigint, "volume" bigint NOT NULL, "symbol" text NOT NULL, "change" text, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d6303a200d1d9dc8f3bcb857331" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "user"."users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "password" character varying NOT NULL, "username" character varying(200) NOT NULL, "full_name" character varying, "phone" character varying, "gender" integer, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "verification_code" character varying, "verification_time" numeric, "token" character varying, CONSTRAINT "username" UNIQUE ("username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "user"."users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "password" character varying NOT NULL, "username" character varying(200) NOT NULL, "email" character varying(200), "full_name" character varying, "phone" character varying, "gender" integer, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "verification_code" character varying, "verification_time" numeric, "token" character varying, CONSTRAINT "username" UNIQUE ("username"), CONSTRAINT "user_email" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "user"."transaction" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "amount" numeric NOT NULL, "amount_before" numeric, "amount_after" numeric, "status" character varying NOT NULL DEFAULT 'NEW REQUEST', "name" character varying, "note" character varying(4000), "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT now(), "updatedAt" TIMESTAMP DEFAULT now(), "checkout_url" character varying, "status_url" character varying, "address" character varying, "txn_id" character varying, "confirms_needed" character varying, "timeout" integer, "type" character varying, "mail" character varying, "statusSendMail" character varying(4000), "transaction_code" character varying, "method" character varying, "userId" uuid, CONSTRAINT "UQ_9a479da886a2af3f4dac001e1f4" UNIQUE ("transaction_code"), CONSTRAINT "PK_89eadb93a89810556e1cbcd6ab9" PRIMARY KEY ("id"))`,
