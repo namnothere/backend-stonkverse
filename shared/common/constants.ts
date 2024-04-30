9ed6d5f1fe220d2ea5e634fe70d983d4508bef15
Nam <canhnamtran216@email.com>
2024-04-30 17:14:34 +0700

    feat: common contstants

---
+++ shared/common/constants.ts
@@ -0,0 +1,12 @@
+export enum RESULT_STATUS {
+  FAILED = 0,
+  SUCCESS = 1,
+}
+
+export enum MESSAGES {
+  SUCCESS = "Success",
+  FAILED = "Failed",
+  NOT_FOUND = "Not Found",
+  USER_NOT_FOUND = "User not found",
+  LEARNING_PROGRESS_NOT_FOUND = "Learning progress not found",
+}
