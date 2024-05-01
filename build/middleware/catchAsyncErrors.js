b576c1a185402e57461735053b5303e7200f4539
Nam <canhnamtran216@email.com>
2024-05-01 15:24:25 +0700

    feat: remove build

--- build/middleware/catchAsyncErrors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CatchAsyncErrors = void 0;
-const CatchAsyncErrors = (theFunc) => (req, res, next) => {
-    Promise.resolve(theFunc(req, res, next)).catch(next);
-};
-exports.CatchAsyncErrors = CatchAsyncErrors;
